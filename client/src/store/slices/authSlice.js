09fd57a1a621adb3f41dff4878d5de8bd6da4fe6
KrupaRaju-79 <achchanakruparaju@gmail.com>
2025-12-23 22:58:40 +0530

    updated authSlile.js with registraionUser Thunk

--- client/src/store/slices/authSlice.js
+++ client/src/store/slices/authSlice.js
@@ -17,3 +17,42 @@ export const  loginUser = createAsyncThunk(
         }
     }
 );
+export const registerUser = createAsyncThunk(
+    'auth/register',
+    async (userData, { rejectWithValue }) => {
+        try {
+            const { data } = await axios.post('/auth/register', userData);
+            if (data.token) {
+                localStorage.setItem('token', data.token);
+                localStorage.setItem('user', JSON.stringify(data.user));
+            }
+            return data;
+        }
+        catch (error) {
+            return rejectWithValue(error.response?.data?.message || 'Registration failed')
+        }
+    }
+);
+
+export const logoutUser = createAsyncThunk(
+    'auth/logout',
+    async (_, { rejectWithValue }) => {
+        try {
+            await axios.post('/auth/login');
+            localStorage.removeItem('token');
+            localStorage.removeItem('user');
+            return null;
+        }catch (error) {
+            localStorage.removeItem('token');
+            localStorage.removeItem('user');
+            return isRejectedWithValue(error.response?.data?.message || 'Logout failed');
+        }
+    }
+);
+const initialState = {
+    user: JSON.parser(localStorage.getItems('user')) || null,
+    token: localStorage.getItem('token') || null,
+    isLoading: false,
+    error: null,
+    isAuthenticated: !!localStorage.getItem('token'),
+};
